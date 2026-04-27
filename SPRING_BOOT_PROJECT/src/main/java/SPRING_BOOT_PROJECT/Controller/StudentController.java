5b013c3fa8aa847a6ceb0d41411f7245877919bc
di763 <dileepkrmuz1121@gmail.com>
2026-04-27 23:28:15 +0530

    Updated StudentController

--- SPRING_BOOT_PROJECT/src/main/java/SPRING_BOOT_PROJECT/Controller/StudentController.java
+++ SPRING_BOOT_PROJECT/src/main/java/SPRING_BOOT_PROJECT/Controller/StudentController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/students")
+@RequestMapping("/student")
 public class StudentController {
 
     private final StudentService studentService;
